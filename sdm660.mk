c439a63cab72b0d00e03f386182aea772567cb41
xyz-sundram <sundrraam@gmail.com>
2024-02-24 17:49:43 +0530

    sdm660-common: drop xiaomi doze and parts

--- sdm660.mk
+++ sdm660.mk
@@ -173,14 +173,6 @@ PRODUCT_COPY_FILES += \
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
-# DeviceSettings
-PRODUCT_PACKAGES += \
-   DeviceSettings
-
-# DeviceDoze
-PRODUCT_PACKAGES += \
-    DeviceDoze
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
